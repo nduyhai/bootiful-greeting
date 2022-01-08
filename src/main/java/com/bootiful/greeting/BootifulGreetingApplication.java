f6924c076039358f627319f41230dafea8970620
Hai Nguyen <haind.vng@gmail.com>
2022-01-08 19:29:46 +0700

    init project

---
+++ src/main/java/com/bootiful/greeting/BootifulGreetingApplication.java
@@ -0,0 +1,13 @@
+package com.bootiful.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulGreetingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootifulGreetingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/bootiful/greeting/BootifulGreetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootiful.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifulGreetingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
